7528a781fc927571213100d937130adc838199e4
danPixl <d@danpixl.me>
2017-07-07 23:21:56 -0600

    combine everything for easier hashing

--- js/ourCode.js
+++ js/ourCode.js
@@ -1,3 +1,17 @@
+function randString() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_+=`~\|][{}';/<>.,/";
+
+  for (var i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+var pattern = GeoPattern.generate(randString(), {color: '#028090'})
+
+document.getElementById("patternDisplay").style.backgroundImage = pattern.toDataUrl()
+
 function randColor() {
 	var colors = ['#093518', '#360B11', '#29041D']
 	return colors[Math.floor(Math.random() * colors.length)]
