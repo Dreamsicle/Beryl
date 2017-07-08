125bb43f7ca240fbfcde463521c4e3ee283d1dc6
danPixl <d@danpixl.me>
2017-07-07 22:58:05 -0600

    stuff

---
+++ js/colorSpecific/randDarkColors.js
@@ -0,0 +1,20 @@
+function randColor() {
+	var colors = ['#093518', '#360B11', '#29041D']
+	return colors[Math.floor(Math.random() * colors.length)]
+}
+
+function randString() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_+=`~\|][{}';/<>.,/";
+
+  for (var i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+var sessionRandColor = randColor()
+
+var pattern = GeoPattern.generate(randString(), {color: sessionRandColor})
+
+document.getElementById("thisIsJustForThePattern").style.backgroundImage = pattern.toDataUrl()
